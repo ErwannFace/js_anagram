4a2558bb919e187f9c1d46b449c1e4e192e4109e
vv221 <vv221@dotslashplay.it>
2016-07-09 18:13:01 +0200

    first working prototype

--- anagram.js
+++ anagram.js
@@ -11,7 +11,43 @@ function Anagram(word) {
 }
 
 Anagram.prototype.matches = function (words) {
-//
-// YOUR CODE GOES HERE
-//
+	if (typeof words == 'string') { words = arguments; }
+	var anagramsList = [];
+	var anagramsListIndex = 0;
+	var tableRef = this.countChar(this.word)
+	for (var i = 0; i < words.length; i++) {
+		if (this.word.toLowerCase() !== words[i].toLowerCase()) {
+			var tableWord = this.countChar(words[i]);
+			if (this.compareTables(tableRef,tableWord)) {
+				anagramsList[anagramsListIndex] = words[i];
+				anagramsListIndex++;
+			}
+		}
+	}
+	return anagramsList;
+}
+
+Anagram.prototype.countChar = function (str) {
+	var table = [];
+	for (var i = 0; i < str.length; i++) {
+		var index = str.charAt(i).toLowerCase();
+		if (! table[index]) {
+			table[index] = 1;
+		} else {
+			table[index]++;
+		}
+	}
+	console.log(str);
+	console.log(table);
+	return table;
+}
+
+Anagram.prototype.compareTables = function (table1,table2) {
+	for (var x in table1) {
+		if (! table2[x] || table1[x] !== table2[x]) { return false; }
+	}
+	for (var x in table2) {
+		if (! table1[x] || table2[x] !== table1[x]) { return false; }
+	}
+	return true;
 }
