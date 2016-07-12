f669470c62249bdb7ef369f3d6aee649b2082518
Lise Poirier <lise.p.poirier@gmail.com>
2016-07-12 10:12:01 +0200

    Nettoyage code

--- anagram.js
+++ anagram.js
@@ -9,39 +9,29 @@
 function Anagram(word) {
     this.word = word;
     this.mot = this.trier(this.word);
-    //ligne permettant de trier le motAlpha
 }
 
 Anagram.prototype.trier = function (trie) {
     var motAlpha = trie.toLowerCase().split("").sort().join("");
-    /* fonction universelle pour trier un mot ===
-    var motAlpha = this.trie.toLowerCase();
-    var motAlpha = motAlpha.split("");
-    var motAlpha = motAlpha.sort();
-    var motAlpha = motAlpha.join(""); */
     return motAlpha;
 }
 
 Anagram.prototype.matches = function (words) {
 
     if (typeof words == "string") {
-            words = arguments;
-        } //Permet de faire passer la string en "tableau"
+        words = arguments;
+    }
 
-    var matches = []; //variable pour tableau final
-    var i = 0; //var pour parcourir notre tableau
+    var matches = [];
+    var i = 0;
 
-        for (i; i < words.length; i++) { /*condition pour parcourir le tableau
-            avec l'argument de taille pour ne pas qu'il aille plus loin que la longueur du tableau*/
-            if (words[i].toLowerCase() != this.word.toLowerCase()) {/*comparaison entre Maj
-                et minuscule (pour le test Banana)*/
-                if (this.trier(words[i]) === this.mot) {
-                //condition de comparaison entre motAlpha trié et motTableau trié
-                matches.push(words[i]);//ajout des anagrammes au tableau final
+    for (i; i < words.length; i++) {
+        if (words[i].toLowerCase() != this.word.toLowerCase()) {
+            if (this.trier(words[i]) === this.mot) {
+                matches.push(words[i]);
             }
         }
+    }
 
-        }
-        /*alert("c." + matches); Permet de voir si je retourne bien mon putain de tableau*/
-        return matches;//return le tableau final
+    return matches;
 }
