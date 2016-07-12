021e786a9f01a41a8a35a26172afc3228e9f6ff2
hassarennes <hassanrennes@gmail.com>
2016-07-12 23:33:00 +0200

    script anagram

--- anagram.js
+++ anagram.js
@@ -1,17 +1,32 @@
-//
-// This is only a SKELETON file for the "Anagram" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
 
-'use strict';
-
-function Anagram(word) {
-  this.word = word;
+function Anagram(mot) {
+ this.mot = mot;
 }
 
-Anagram.prototype.matches = function (words) {
-//
-// YOUR CODE GOES HERE
-//
+Anagram.prototype.matches = function Anagram (mots) {
+if(typeof(mots) === "string") {
+   if(unAnagrams(mots, this.mot)) {
+   return [mots];
+   }
 }
+else {
+    return mots.filter(function(mot) {
+    return unAnagrams(this.mot, mot);
+    }, this);    
+}
+return mots.filter(function(mot) {
+    return unAnagrams(this.mot, mot);
+}, this);
+};
+
+function unAnagrams(premierMots, deuxiemeMots) {
+    premierMots = premierMots.toLowerCase();
+    deuxiemeMots = deuxiemeMots.toLowerCase();
+    return premierMots !== deuxiemeMots && memeLettre(premierMots, deuxiemeMots);
+}
+function memeLettre(premierMots, deuxiemeMots) {
+    return trie(premierMots) == trie(deuxiemeMots);
+}
+
+function trie(str) {
+    return str.split('').sort().join('');
