b09cfe988a905669bce54272de7c3eed2b726255
hassanrennes <hassanrennes@gmail.com>
2016-07-13 10:00:59 +0200

    ajout de commentaire

--- anagram.js
+++ anagram.js
@@ -4,14 +4,31 @@
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
 
-'use strict';
-
-function Anagram(word) {
-  this.word = word;
+function Anagram(mot) {
+ this.mot = mot;
+ // La méthode match() permet d'obtenir le tableau des correspondances entre la chaîne courante et une expression rationnelle.
+}Anagram.prototype.matches = function Anagram (mots) {
+if(typeof(mots) === "string") {
+	// Sont anagrammes (mots, this.word)? retourner mots: return '';
+   if(unAnagrams(mots, this.mot)) {
+   return [mots];
+   }
 }
-
-Anagram.prototype.matches = function (words) {
-//
-// YOUR CODE GOES HERE
-//
+// L'opérateur typeof renvoie une chaîne qui indique le type de son opérande.
+else {
+    return mots.filter(function(mot) {
+    return unAnagrams(this.mot, mot);
+    }, this);    
 }
+return mots.filter(function(mot) {
+    return unAnagrams(this.mot, mot);
+}, this);
+};function unAnagrams(premierMots, deuxiemeMots) {
+    premierMots = premierMots.toLowerCase();
+    deuxiemeMots = deuxiemeMots.toLowerCase();
+    return premierMots !== deuxiemeMots && memeLettre(premierMots, deuxiemeMots);
+}
+function memeLettre(premierMots, deuxiemeMots) {
+    return trie(premierMots) == trie(deuxiemeMots);
+}function trie(str) {
+    return str.split('').sort().join('');
