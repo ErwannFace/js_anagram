0681ebbe7cfdaadedeb7da105a91f082d9d26880
Megaju <julien_malle@outlook.fr>
2016-07-13 17:26:35 +0200

    First and last commit

--- anagram.js
+++ anagram.js
@@ -7,11 +7,42 @@
 'use strict';
 
 function Anagram(word) {
-  this.word = word;
+    this.word = word;
+}
+
+Anagram.prototype.tri = function (mot) {
+    //fonction de tri
+    var str = mot.toLowerCase();
+    str = str.split('');
+    str = str.sort();
+    str = str.join('');
+    console.log(str);
+    return str;
 }
 
 Anagram.prototype.matches = function (words) {
-//
-// YOUR CODE GOES HERE
-//
+    var drow = this.tri(this.word); //mon mot traité
+    var i = 0; //pour la boucle for (note:obligé de la déclarer à cause de 'use strict').
+    var matches = []; //tableau dans lequel on va enreigistrer les elements si bien des anagrammes
+    
+    
+    if (typeof words === 'string'){
+        words = arguments; // on transforme le string temporerement en tableau car il ne tri que des tableaux
+    }
+    
+    var t = words.length; //nombres d'éléments dans matches
+    for (i; i < t; i++) {
+        var wordTemp = words[i];
+        var drows = this.tri(wordTemp);//wordTemp.split('').sort().join('').trim().toLowerCase(); //les elements traités
+
+        if (drows === drow && this.word.toUpperCase() != wordTemp.toUpperCase()) {
+            //c'est un anagramme
+            matches.push(wordTemp);
+            
+            console.log(matches);
+        }    
+    } 
+    
+    return matches;
+
 }
